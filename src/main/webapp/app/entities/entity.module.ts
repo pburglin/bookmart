e186cd61d93253c7c9a686da2a920086df52145b
Pedro Burglin <pedro.burglin@gmail.com>
2017-12-13 21:06:37 -0500

    Added Book entity

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { BookmartAuthorModule } from './author/author.module';
+import { BookmartBookModule } from './book/book.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         BookmartAuthorModule,
+        BookmartBookModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
